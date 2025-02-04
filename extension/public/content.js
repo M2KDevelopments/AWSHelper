e14b42c0cef57ccf0c31cd31cba987d5f99ce010
M2K Developments <m2ksoftiez@gmail.com>
2025-02-04 17:14:22 +0200

    Look for an element from a text inside an attribute

--- extension/public/content.js
+++ extension/public/content.js
@@ -1,1 +1,20 @@
 console.log("Running AWSHelper")
+
+
+
+/**
+ * Look for an element from a text inside an attribute
+ * @param {String} searchAttrText What text of the attribute to search for
+ * @param {Element} parentElement which parent element to start from. 
+ * @param {String} tag which element tag to look for
+ * @param {String} attr which attribute to look for 
+ * @returns 
+ */
+function getElement(searchAttrText, parentElement = null, tag = 'div', attr = "class") {
+    const divs = (parentElement || document).querySelectorAll(tag);
+    for (const div of divs) {
+        if (div.getAttribute(attr) && div.getAttribute(attr).includes(searchAttrText)) {
+            return div;
+        }
+    }
+}
